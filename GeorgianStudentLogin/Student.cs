fe93e4b5034abcc69eecff8e535124514a95c17f
RupaliChugh1 <100984286+RupaliChugh1@users.noreply.github.com>
2022-03-05 00:19:08 -0500

    Add files via upload

---
+++ GeorgianStudentLogin/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgianStudentLogin
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public String Class { get; set; }
+        public String emailID { get; set; }
+        public String phoneNumber { get; set; }
+        public String city { get; set; }
+        public String country { get; set; }
+        public String MarksObtained { get; set; }
+
+
+
+
+
+
+    }
+}
